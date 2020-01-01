4eaa425f7b11a23d1c514dd66378182e7d59b7b5
Alvin Febriando <alvinfebriando@outlook.com>
2020-01-01 14:09:01 +0700

    Populate bingo randomly 1-25

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Util from './util/util';
+
+import Bingo from './components/Bingo';
 import './App.css';
 
-function App() {
+const numArr = [];
+const checkedArr = [];
+
+for (let i = 1; i <= 25; i++) {
+  numArr.push(i);
+  checkedArr.push(false);
+}
+
+const App = () => {
+  const [num1, setNum1] = useState(Util.shuffle(numArr));
+  const [num2, setNum2] = useState(Util.shuffle(numArr));
   return (
     <div id="main">
       <h1>Hello World</h1>
+      <Bingo number={num1}></Bingo>
+      <Bingo number={num2}></Bingo>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/util/util.js
@@ -0,0 +1,16 @@
+const shuffle = originalArray => {
+  let array = [...originalArray];
+  let counter = array.length;
+
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
+    counter--;
+
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+  return array;
+};
+
+export default { shuffle };
