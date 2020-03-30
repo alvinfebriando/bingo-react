c48c5d9d6113d8d8e03000322b5ca10d60224a88
Alvin Febriando <alvinfebriando@outlook.com>
2020-03-30 09:48:24 +0700

    Updates

---
+++ precache-manifest.4725bcaa902a85eafae64543f43550ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ebb44fe611244d1e6db0c424c2c944",
+    "url": "/bingo-react/index.html"
+  },
+  {
+    "revision": "8b9bb2ab876a6e534096",
+    "url": "/bingo-react/static/css/main.3552b71c.chunk.css"
+  },
+  {
+    "revision": "bb0d2a444f1deaf03bb7",
+    "url": "/bingo-react/static/js/2.4a3ce77b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bingo-react/static/js/2.4a3ce77b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b9bb2ab876a6e534096",
+    "url": "/bingo-react/static/js/main.b8db63e0.chunk.js"
+  },
+  {
+    "revision": "42f2e994f06d6bcc30f9",
+    "url": "/bingo-react/static/js/runtime-main.a57e045f.js"
+  }
+]);
